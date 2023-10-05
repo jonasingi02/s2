2f0d94b3bf04437bb5a7e755188bc1620dafdbe7
Dagurko <112619676+Dagurko@users.noreply.github.com>
2023-10-05 21:57:43 +0000

    Fast added

--- Fast.java
+++ Fast.java
@@ -1,15 +1,10 @@
 import java.util.Arrays;
-// import java.util.Comparator;
-// import edu.princeton.cs.algs4.StdDraw;
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.Out;
-
 import java.util.ArrayList;
-//import java.util.HashSet;
+import java.util.HashSet;
 import java.util.List;
-// import java.util.Set;
+import java.util.Set;
 
-// import javax.swing.text.Segment;
+
 
 public class Fast {
     private final List<List<Point>> segments;
@@ -17,6 +12,38 @@ public class Fast {
     public Fast(Point[] points) {
         segments = new ArrayList<>();
         Arrays.sort(points);
+        int n = points.length;
+        Set<String> printedSegments = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            Point[] sortedPoints = Arrays.copyOf(points, n);
+            Arrays.sort(sortedPoints, points[i].SLOPE_ORDER);
+
+            int j = 1;
+            while (j < n) {
+                int k = j + 1;
+                while (k < n && points[i].slopeTo(sortedPoints[j]) == points[i].slopeTo(sortedPoints[k])) {
+                    k++;
+                }
+
+                if (k - j >= 3) {
+                    List<Point> segment = new ArrayList<>();
+                    segment.add(points[i]);
+                    for (int m = j; m < k; m++) {
+                        segment.add(sortedPoints[m]);
+                    }
+                    segment.sort(null); // Sort the segment lexicographically
+                    String segmentString = segment.toString();
+
+                    if (!printedSegments.contains(segmentString)) {
+                        segments.add(segment);
+                        printedSegments.add(segmentString);
+                    }
+                }
+
+                j = k;
+            }
+        }
         
     }
 
@@ -29,17 +56,7 @@ public class Fast {
     }
 
 
-
     public static void main(String[] args) {
-        // In in = new In();
-        // Out out = new Out();
-        // int n = in.readInt();
-        // Point[] points = new Point[n];
-        // for (int i = 0; i < n; i++) {
-        //     int x = in.readInt(), y = in.readInt();
-        //     points[i] = new Point(x, y);
-        // }
-        // brute(points);
         Point[] points = new Point[15];
         points[0] = new Point(10,0);
         points[1] = new Point(8,2);
@@ -56,69 +73,15 @@ public class Fast {
         points[12] = new Point(11,3);
         points[13] = new Point(5,12);
         points[14] = new Point(9,6);
-
-        Brute brute = new Brute(points);
-        for (List<Point> segment : brute.segments()) {
-            System.out.println(segment);
+    
+        Fast fast = new Fast(points);
+        for (List<Point> segment : fast.segments()) {
+            System.out.print(segment.get(0)); // Print the first point in the segment
+            for (int i = 1; i < segment.size(); i++) {
+                System.out.print(", " + segment.get(i)); // Print the rest of the points
+            }
+            System.out.println(); // Move to the next line after printing a segment
         }
     }
 }
-    // public List<List<Point>> segments(Point[] points) {
-    //     List<List<Point>> lineSegments = new ArrayList<>();
-        
-    //     // Iterate through each point as the reference point.
-    //     for (Point p : points) {
-    //         Point[] sortedPoints = Arrays.copyOf(points, points.length); // Create a copy
-    //         Arrays.sort(sortedPoints, p.SLOPE_ORDER);// Sort by slope relative to p
-            
-    //         // Implement the logic to find collinear segments and add them to lineSegments.
-    //         // Be careful to avoid duplicates.
-    //     }
-        
-    //     return lineSegments;
-    // }
-
-    // public static void main(String[] args) {
-    //     In in = new In();
-    //     Out out = new Out();
-    //     int n = in.readInt();
-    //     Point[] points = new Point[n];
-    //     for (int i = 0; i < n; i++) {
-    //         int x = in.readInt(), y = in.readInt();
-    //         points[i] = new Point(x, y);
-    //     }
-
-    //     // Call the segments method to find collinear segments
-    //     Fast fast = new Fast();
-    //     List<List<Point>> lineSegments = fast.segments(points);
-
-    //     // Sort the line segments as per requirements
-    //     lineSegments.sort((segment1, segment2) -> {
-    //         // Sort by the first point in each line segment
-    //         int cmp = segment1.get(0).compareTo(segment2.get(0));
-    //         if (cmp != 0) {
-    //             return cmp;
-    //         }
-            
-    //         // If the first points are equal, compare by slope of the patterns
-    //         double slope1 = segment1.get(0).slopeTo(segment1.get(segment1.size() - 1));
-    //         double slope2 = segment2.get(0).slopeTo(segment2.get(segment2.size() - 1));
-    //         return Double.compare(slope1, slope2);
-    //     });
-
-    //     // Print the sorted line segments
-    //     for (List<Point> segment : lineSegments) {
-    //         for (int i = 0; i < segment.size(); i++) {
-    //             out.print(segment.get(i));
-    //             if (i < segment.size() - 1) {
-    //                 out.print(" -> ");
-    //             }
-    //         }
-    //         out.println();
-    //     }
-    // }
-
-    
-  
-
 
